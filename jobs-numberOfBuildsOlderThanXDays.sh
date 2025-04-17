891b8b48575f49c65ab654cf690c5073123cb3b4
cccaternberg <acaternberg@cloudbees.com>
2025-04-17 12:49:26 +0200

    add

--- jobs-numberOfBuildsOlderThanXDays.sh
+++ jobs-numberOfBuildsOlderThanXDays.sh
@@ -7,5 +7,7 @@ def cutoffTime = System.currentTimeMillis() - (thresholdDays * 24 * 60 * 60 * 10
 
 Jenkins.instance.getAllItems(Job).each { job ->
     def oldBuilds = job.builds.findAll { it.timeInMillis < cutoffTime }
-    println "Job: ${job.fullName} has ${oldBuilds.size()} builds older than ${thresholdDays} days."
+    if (oldBuilds.size > 0){
+       println "Job: ${job.fullName} has ${oldBuilds.size()} builds older than ${thresholdDays} days."
+   }
 }
